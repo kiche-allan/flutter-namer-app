6a6b16ed5883f3e7daa3b67a032341cbfa97d063
Allan Kiche <tle2819@students.mu.ac.ke>
2023-03-01 00:04:10 +0300

    flutter7

--- lib/main.dart
+++ lib/main.dart
@@ -166,6 +166,7 @@ class _MyHomePageState extends State<MyHomePage> {
 class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     var appState = context.watch<MyAppState>();
 
     if (appState.favorites.isEmpty) {
@@ -174,23 +175,46 @@ class FavoritesPage extends StatelessWidget {
       );
     }
 
-    return ListView(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
+          padding: const EdgeInsets.all(30),
+          child: Text(
+            'You have'
+            '${appState.favorites.length} favorites:'
           ),
+        ),
+
+        Expanded(
+           child: GridView(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 400 / 80,
+        ), 
+           children: [
+              for (var pair in appState.favorites)
+                ListTile(
+                  leading: IconButton(
+                    icon: Icon(Icons.delete_outline, semanticLabel: 'Delete'),
+                    color: theme.colorScheme.primary,
+                    onPressed: () {
+                      appState.removeFavorite(pair);
+                    },
+                  ),
+                  title: Text(
+                    pair.asLowerCase,
+                    semanticsLabel: pair.asPascalCase,
+                  ),
+                ),
+            ],
+          ),
+        ),
       ],
     );
   }
 }
-
+    
 class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
